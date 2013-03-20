a8d83c02069211fefe64f5cf8abc25fab9b15776
Jonathan Nilsson <lord.jonil@gmail.com>
2013-03-20 18:22:33 +0100

    Built format length code

--- include/jpack.h
+++ include/jpack.h
@@ -1,29 +1,35 @@
-/*******************************************************************************
- *
- * Copyright (C) 2013 Jonathan Nilsson
- * Contact: l.a.jonathan.nilsson@gmail.com
- *
- * This file is part of JPack.
- *
- * JPack is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- ******************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* Copyright (C) 2013 Jonathan Nilsson                                        */
+/* Contact: l.a.jonathan.nilsson@gmail.com                                    */
+/*                                                                            */
+/* This file is part of JPack.                                                */
+/*                                                                            */
+/* JPack is free software: you can redistribute it and/or modify              */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation, either version 3 of the License, or          */
+/* (at your option) any later version.                                        */
+/*                                                                            */
+/* This program is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/* GNU General Public License for more details.                               */
+/*                                                                            */
+/******************************************************************************/
 
 #ifndef JPACK_H_
 #define JPACK_H_
 
 #include <stdlib.h>
 
+// Packs the data given into the buffer according to the format
 // Returns the number of bytes written to buf. If buf was to short it returns
 // the number of bytes that would have been written if there was enought space
+
+// Option, description
+// <       little-endian (default)
+// >       big-endian
+// !       network (big)
 
 // Format char, type,     length
 // x,           padbyte,  1
@@ -40,6 +46,12 @@
 // s,           string,   -
 
 int jpack(char * buf, size_t size, const char * format, ...);
+
+// Unpacks the buffer according to the format to the addresses provided
 void junpack(const char * buf, size_t size, const char * format, ...);
 
-#endif
+// Returns the buffer needed to hold the format or -1 on invalid format and 
+// -2 if the resault is unknown i.e. the format contains a string
+int jpack_format_length(const char * format);
+
+#endif // JPACK_H_
--- src/jpack.c
+++ src/jpack.c
@@ -1,40 +1,118 @@
-/*******************************************************************************
- *
- * Copyright (C) 2013 Jonathan Nilsson
- * Contact: l.a.jonathan.nilsson@gmail.com
- *
- * This file is part of JPack.
- *
- * JPack is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- ******************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/* Copyright (C) 2013 Jonathan Nilsson                                        */
+/* Contact: l.a.jonathan.nilsson@gmail.com                                    */
+/*               	                                                             */
+/* This file is part of JPack.                                                */
+/*                                                                            */
+/* JPack is free software: you can redistribute it and/or modify              */
+/* it under the terms of the GNU General Public License as published by       */
+/* the Free Software Foundation, either version 3 of the License, or          */
+/* (at your option) any later version.                                        */
+/*                                                                            */
+/* This program is distributed in the hope that it will be useful,            */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/* GNU General Public License for more details.                               */
+/*                                                                            */
+/******************************************************************************/
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#ifdef TEST
 #include <stdio.h>
+#endif // TEST
 
 #include "jpack.h"
 
 int jpack(char * buf, size_t size, const char * format, ...) {
-	(void)buf; (void)size; (void)format;
+	uint32_t offset = 0;
+
+
+
+
 	return 0;
 }
 
 void junpack(const char * buf, size_t size, const char * format, ...) {
 	(void)buf; (void)size; (void)format;
+
+	uint32_t offset = 0;
+}
+
+int jpack_format_length(const char * format) {
+
+	if (format == NULL) {
+		return 0;
+	}
+
+	int length = 0;
+
+	while (format++) {
+		switch (*format) {
+			case 'x':
+			case 'b':
+			case 'B':
+				length += 1;
+				break;
+			case 'h':
+			case 'H':
+				length += 2;
+				break;
+			case 'i':
+			case 'I':
+			case 'f':
+				length += 4;
+				break;
+			case 'l':
+			case 'L':
+			case 'd':
+				length += 8;
+				break;
+			case '<':
+			case '>':
+			case '!':
+				break;
+			default:
+				return -1;
+		}
+	}
+
+	return length;
+	
 }
 
 #ifdef TEST
 int main(int argc, char *argv[]) {
 	(void)argc; (void)argv;
-	printf("Hej");
-	return 0;
+	
+	if (jpack_format_length("IiB") != 9) {
+		fprintf(stderr, "Failed to get buffer length for format");
+		return EXIT_FAILIURE;
+	}
+
+	char buffer[9];
+
+	uint32_t a = 1, a2;
+	int32_t b = -1, b2;
+	uint8_t c = 1, c2;
+
+	if (jpack(buffer, 9, "IiB", a, b, c) < 0) {
+		fprintf(stderr, "Failed to pack data");
+		return EXIT_FAILIURE;
+	}
+
+	junpack(buffer, 9, "IiB", &a2, &b2, &c2);
+
+	if (a != a2 ||
+	    b != b2 ||
+	    c != c2) {
+		fprintf(stderr, "Unpacked data is not the same as packed data");
+		return EXIT_FAILIURE;
+	}
+
+	return EXIT_SUCCESS;
 }
-#endif
+#endif // TEST
